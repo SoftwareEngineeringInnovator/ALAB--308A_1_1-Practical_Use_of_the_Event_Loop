5168d7e6d16a36aa12f7c6ddca9335cec849cfec
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-06-02 22:17:29 -0400

    develop the recursive stack overflow function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("ALAB 308A.1.1 - Practical Use of the Event Loop");
+
+
+// PART 1: STACK OVERFLOW
+
+// This counter keeps track of how many times the function runs.
+let stackCounter = 0;
+
+// function causeStackOverflow() {
+//     while (true) {
+//         stackCounter++;
+//     }
+// }
+
+// This function to be used as recursion.
+function causeStackOverflow() {
+    stackCounter++;
+    causeStackOverflow();
+}
+
+// causeStackOverflow();
